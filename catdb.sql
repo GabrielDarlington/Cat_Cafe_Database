71c49a774c50e3e7fb3c5e2d48937d6545d5d4a5
GabrielDarlington <96272656+GabrielDarlington@users.noreply.github.com>
2023-04-05 18:40:16 -0600

    Update catdb.sql

--- catdb.sql
+++ catdb.sql
@@ -1,2 +1,62 @@
 CREATE DATABASE catcafe;
 
+CREATE TABLE catcafe.dbo.CafeInventory
+(
+
+);
+
+CREATE TABLE catcafe.dbo.Ingredients
+(
+
+);
+
+CREATE TABLE catcafe.dbo.Employee_pay
+(
+
+);
+
+CREATE TABLE catcafe.dbo.Schedule
+(
+
+);
+
+CREATE TABLE catcafe.dbo.Employee
+(
+
+);
+
+
+CREATE TABLE catcafe.dbo.CafeMenu
+(
+
+);
+
+CREATE TABLE catcafe.dbo.CafePurchases
+(
+
+);
+
+CREATE TABLE catcafe.dbo.Adoptions
+(
+
+);
+
+CREATE TABLE catcafe.dbo.MembershipSignup
+(
+
+);
+
+CREATE TABLE catcafe.dbo.Cats
+(
+
+);
+
+CREATE TABLE catcafe.dbo.Veterenarians
+(
+
+);
+
+
+
+
+
