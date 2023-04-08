1572964d1280600bf45b93de39d55c27c64539bc
Brayden <braydenchristensen6@gmail.com>
2023-04-07 23:57:36 -0600

    Changes to be committed: 	new file:   Trees.txt <- for street names 	new file:   Utah_cities.txt < ignore this 	new file:   Utah_specific_streets.txt <- for street names 	new file:   employees.txt <- employee data 	new file:   first_name.txt <- for first names 	new file:   last_name.txt <- for last names 	new file:   members.txt <- members data 	new file:   person.py <- generator script 	new file:   utah_cities.csv <- utah cities

---
+++ person.py
@@ -0,0 +1,90 @@
+import re
+import os
+import pandas as pd
+from random import randint, randrange, choice
+
+def phone_number():
+    first_three = randrange(100,999)
+    last_four = randrange(1000,9999)
+    area_code = choice([385,435,801])
+    phone = f"{area_code}{first_three}{last_four}"
+    
+    return phone
+
+def email(first: str, last: str) -> str:
+    domains = ["gmail.com", "outlook.com", "yahoo.com", "godaddy.com", "comcast.net",
+               "apple.com", "notafakeemail.net", "iloveanimals.org"]
+    email_format = randint(0,20)
+
+    if email_format < 5:
+        return f"{first}.{last}@{choice(domains)}"
+    elif email_format >= 5 and email_format <= 10:
+        return f"{first[1]}.{last}@{choice(domains)}"
+    elif email_format >= 11 and email_format <=20:
+        return f"{first}.{last}{randint(0,9999)}@{choice(domains)}"
+    elif email_format > 20:
+        return f"{first}{last[3]}@{choice(domains)}"
+
+def address():
+    compass = ["N","E","S","W"]
+    street_suff = ["Dr", "Ave", "Cir", "Blvd", "Way", "St"]
+    df = pd.read_csv("utah_cities.csv")
+    street1 = open("trees.txt").read().split()
+    street2 = open("Utah_specific_streets.txt").read().split()
+    zip, city, state = df.iloc[randint(1,len(df)-1)]
+    street_choice = randint(1,4)
+
+    match street_choice:
+        case 1:
+            return [f"{randint(0,9000)} {choice(compass)} {choice(street2)}", city, state, zip]
+        case 2:
+            return [f"{randint(0,9000)} {choice(compass)} {choice(street2)} {choice(street_suff)}" , city, state, zip]
+        case 3:
+            return [f"{randint(0,9000)} {choice(compass)} {choice(street1).capitalize()}",city, state, zip]
+        case 4:
+            return [f"{randint(0,9000)} {choice(compass)} {choice(street1).capitalize()} {choice(street_suff)}"  , city, state, zip]
+        
+def names():
+    first = open("first_name.txt").read().split()
+    last = open("last_name.txt").read().split()
+    return [choice(first), choice(last)]
+
+
+if __name__ == "__main__":
+     manager_count = 0
+     with open("members.txt", "w") as mem, open("employees.txt", "w") as emp:
+        for i in range(150):
+            first, last = names()
+            street, city, state, zip = address()
+            phone = phone_number()
+            member_email = email(first, last)
+                
+            
+            mem.write(
+                f"INSERT INTO catcafe.dbo.MemberSignup VALUES('{first}','{last}','{phone}','{street}','{city}','{state}','{zip}')\n"
+                )
+        
+        for i in range(15):
+            first, last = names()
+            street, city, state, zip = address()
+            phone = phone_number()
+            is_manager = randint(0,1)
+            sick_days = randint(0,15)
+            vacation_days = randint(0,30)
+
+            if is_manager and manager_count < 2:
+                manager_count += 1
+                emp.write(
+                f"""INSERT INTO catcafe.dbo.Employee VALUES('{first}','{last}','{phone}','{street}','{city}','{state}','{zip}',1,0,{sick_days},{vacation_days})\n"""
+                )
+            else:
+                is_volunteer = randint(0,1)
+                
+                if is_volunteer:
+                    emp.write(
+                        f"""INSERT INTO catcafe.dbo.Employee VALUES('{first}','{last}','{phone}','{street}','{city}','{state}','{zip}',0,1,0,0)\n"""
+                        )
+                else:
+                    emp.write(
+                    f"""INSERT INTO catcafe.dbo.Employee VALUES('{first}','{last}','{phone}','{street}','{city}','{state}','{zip}',0,0,{sick_days},{vacation_days})\n"""
+                    )
